de6abb507ecb917585b6f691231ddcce2e307ebb
ptr <petar0golubovic@gmail.com>
2025-11-22 11:31:45 +0100

    removed the need to import the css file to use the BokReader component with vite-plugin-lib-inject-css

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
+        libInjectCss(),
         dts({
             insertTypesEntry: true,
             entryRoot: "lib",
@@ -32,6 +34,7 @@ export default defineConfig({
                     react: "React",
                     "react-dom": "ReactDOM",
                     "styled-components": "styled",
+                    "react/jsx-runtime": "jsxRuntime",
                 },
             },
         },

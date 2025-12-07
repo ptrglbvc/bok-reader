cde5bd7ff3640aea334bb61887c35941e6248b11
ptr <petar0golubovic@gmail.com>
2025-12-07 15:12:19 +0100

    updated to react 19.2.1

--- src/components/LoadingScreen/LoadingScreen.tsx
+++ src/components/LoadingScreen/LoadingScreen.tsx
@@ -1,23 +1,12 @@
-import { CSSProperties } from "react";
 import styles from "./LoadingScreen.module.css";
-
-// Allow CSS variables in style prop
-interface CustomCSS extends CSSProperties {
-    "--loader-color"?: string;
-}
 
 type LoadingScreenProps = {
     isLoading: boolean;
-    color: string | undefined;
 };
 
 export default function LoadingScreen({
     isLoading,
-    color,
 }: LoadingScreenProps) {
-    const spinnerStyle: CustomCSS = {
-        "--loader-color": color || "red",
-    };
     return (
         <div
             className={
@@ -26,8 +15,7 @@ export default function LoadingScreen({
                     : styles["loading-screen-gon"]
             }
         >
-            <div className={styles.spinner} style={spinnerStyle}>
-                {/* The glowing center dot */}
+            <div className={styles.spinner} >
                 <div className={styles.core}></div>
             </div>
         </div>
